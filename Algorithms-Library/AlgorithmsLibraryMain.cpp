1e6fe927140b5d408085e19f7f0bea7a767c7b73
huozk <huozk0804@126.com>
2020-02-18 15:18:10 +0800

    creat vs project

---
+++ Algorithms-Library/AlgorithmsLibraryMain.cpp
@@ -0,0 +1,12 @@
+// AlgorithmsLibraryMain.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+
+#include <iostream>
+#include <string>
+
+using namespace  std;
+
+int main()
+{
+	cout << "AlgorithmsLibraryMain..." << endl;
+	return 0;
+}
